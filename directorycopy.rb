3da1508dce46995f6237204de72a48debe6a9b1a
Cyryl Gotkowicz <cyrylgotkowicz@gmail.com>
2023-04-05 16:08:40 +0100

    Added center method

--- directorycopy.rb
+++ directorycopy.rb
@@ -4,7 +4,7 @@ def input_students
   students = []
   name = gets.chomp
   while !name.empty? do
-    students << {name: name, cohort: :november}
+    students << {name: name, cohort: :november, grade: 80}
     puts "Now we have #{students.count} students"
     name = gets.chomp
   end
@@ -12,14 +12,14 @@ def input_students
 end
 
 def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
+  puts "The students of Villains Academy".center(100)
+  puts "-------------".center(100)
 end
 
 def print(students)
   count = 0
   while count < students.count do 
-    puts "#{students[count][:name]} (#{students[count][:cohort]} cohort)"
+    puts "#{students[count][:name]} (#{students[count][:cohort]} cohort) grade: #{students[count][:grade]}".center(100)
     count += 1
   end
 end
