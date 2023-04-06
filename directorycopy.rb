fe992f3e86353386b9e0907a1267190710b69aad
Cyryl Gotkowicz <cyrylgotkowicz@gmail.com>
2023-04-06 10:48:45 +0100

    Sorted students by cohort

--- directorycopy.rb
+++ directorycopy.rb
@@ -1,40 +1,31 @@
-def input_students
-  puts "Please enter the name of the first student"
-  students = []
-  name = gets.chomp
-  while !name.empty? do
-    while true do
-      puts "Please enter cohort of student"
-      cohort = gets.chomp
-      if cohort == "november" || cohort == "december"
-        break
-      elsif cohort == ""
-        cohort = "default"
-        break
-      else 
-        cohort.to_sym!
-        puts "Invalid input, try again"
-      end
-    end
-    students << {name: name, cohort: cohort, grade: 80}
-    puts "Now we have #{students.count} students"
-    puts "Please enter the name of the next student"
-    puts "To finish, just hit return again"
-    name = gets.chomp
-  end
-  students
-end
+students = [
+  {name: "Dr. Hannibal Lecter", cohort: :november},
+  {name: "Darth Vader", cohort: :december},
+  {name: "Nurse Ratched", cohort: :december},
+  {name: "Michael Corleone", cohort: :november},
+  {name: "Alex DeLarge", cohort: :november},
+  {name: "The Wicked Witch of the West", cohort: :november},
+  {name: "Terminator", cohort: :december},
+  {name: "Freddy Krueger", cohort: :december},
+  {name: "The Joker", cohort: :november},
+  {name: "Joffrey Baratheon", cohort: :november},
+  {name: "Norman Bates", cohort: :november}
+]
 
 def print_header
   puts "The students of Villains Academy".center(100)
   puts "-------------".center(100)
 end
 
-def print(students)
-  count = 0
-  while count < students.count do 
-    puts "#{students[count][:name]} (#{students[count][:cohort]} cohort) grade: #{students[count][:grade]}".center(100)
-    count += 1
+def print_by_cohort(students)
+  values = students.map { |student| [student["name"], student[:cohort]]}.uniq
+  puts "which of these cohorts would you like to display"
+  puts values
+  choice = gets.chomp
+  students.each do |student|
+    if student[:cohort] == choice.to_sym
+      puts student[:name]
+    end
   end
 end
 
@@ -42,7 +33,5 @@ def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
 
-students = input_students
-print_header
-print(students)
-print_footer(students)
+
+print_by_cohort(students)
